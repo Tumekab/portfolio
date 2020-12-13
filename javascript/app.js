df5d1bf58f28d968132e3d67113dc86852bbe98a
Tumeka Burston <burston1993@icloud.com>
2020-12-13 00:58:55 +0000

    need to fix burger menu and sort out contact grid. other than that coming together nicely.

---
+++ javascript/app.js
@@ -0,0 +1,35 @@
+// window.onscroll = function() {myFunction()};
+
+// // Get the navbar
+// let navbar = document.getElementById("sticky-nav");
+
+// // Get the offset position of the navbar
+// let sticky = navbar.offsetTop;
+
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
+
+
+
+
+// burger function
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  links.forEach(link => {
+    link.classList.toggle("fade");
+  });
+});
+
+// sticky burger menu function
+
+// When the user scrolls the page, execute myFunction

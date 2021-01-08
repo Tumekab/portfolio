f299f737aeead6ead98c6df5567117234b8fcf55
Tumeka Burston <burston1993@icloud.com>
2021-01-08 16:44:51 +0000

    added font awesome icons for links

--- javascript/app.js
+++ javascript/app.js
@@ -1,22 +1,6 @@
-const menuBtn = document.querySelector(".menu-btn");
-const menuItems = document.querySelector(".menu-items");
-const menuItem = document.querySelectorAll(".menu-item");
-
-// main toggle for nav menu
-menuBtn.addEventListener("click", () => {
-  toggle();
-});
-
-//toggle on item, click if open
-menuItem.forEach((item) => {
-  item.addEventListener("click", () => {
-    if(menuBtn.classList.contains("open")) {
-      toggle();
-    };
+$(document).ready(function () {
+  $('.menu-toggler').on('click', function(){
+      $(this).toggleClass('open');
+      $('.top-nav').toggleClass('open');
   })
 });
-
-function toggle(){
-  menuBtn.classList.toggle("open");
-  menuItems.classList.toggle("open");
-}
